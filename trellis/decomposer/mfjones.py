8f96f51ca1c1002038a482bf6ceb58d0681bf6d3
Johannes Fichte <daajoe@ficht.es>
2017-10-11 18:54:17 +0200

    Added: remaining pace solvers

---
+++ trellis/decomposer/mfjones.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env false
+
+from trellis.decomposer.pace import PACEDecomposer
+
+
+class Mfjones2016(PACEDecomposer):
+    name = 'mfjones2016'
+    bin_name = 'mfjones'
+    folder_name = 'mfjones2016'
+
---
+++ trellis/decomposer/mrprajesh.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env false
+
+from trellis.decomposer.pace import PACEDecomposer
+
+
+class Mrprajesh2016(PACEDecomposer):
+    name = 'mrprajesh2016'
+    bin_name = 'mrprajesh'
+    folder_name = 'mrprajesh2016'
